ded7c1fa8fb5ecf299dd74c32b19e46509617411
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2019-02-18 14:14:26 +0000

    Corrected edge residuals for tracer_2d

--- thetis/tracer_2d_residuals.py
+++ thetis/tracer_2d_residuals.py
@@ -24,13 +24,13 @@ class HorizontalAdvectionResidual(TracerTerm):
     def residual_cell(self, solution, solution_old, fields, fields_old, bnd_conditions=None, adjoint=None):
         if fields_old.get('uv_2d') is not None:
             uv = fields_old['uv_2d']
-            P0 = FunctionSpace(solution.function_space().mesh(), "DG", 0)
-            I = TestFunction(P0)
+            i = TestFunction(FunctionSpace(self.mesh, "DG", 0))
 
+            f = 0
             if adjoint is None:
                 f = dot(uv, grad(solution))
             else:
-                f = I * dot(uv, grad(solution)) * adjoint * self.dx
+                f += i * dot(uv, grad(solution)) * adjoint * self.dx
 
             return -f
 
@@ -39,42 +39,41 @@ class HorizontalAdvectionResidual(TracerTerm):
             if adjoint is None:
                 raise NotImplementedError  # TODO
             else:
-                uv = fields_old['uv_2d']
-                P0 = FunctionSpace(solution.function_space().mesh(), "DG", 0)
-                I = TestFunction(P0)
-                i = avg(I)
+                f = 0
+                if self.horizontal_dg:
+                    uv = fields_old['uv_2d']
+                    i = TestFunction(FunctionSpace(self.mesh, "DG", 0))
 
-                uv_av = avg(uv)
-                un_av = (uv_av[0]*self.normal('-')[0]
-                         + uv_av[1]*self.normal('-')[1])
-                s = 0.5*(sign(un_av) + 1.0)
-                c_up = solution('-')*s + solution('+')*(1-s)
+                    uv_av = avg(uv)
+                    un_av = (uv_av[0]*self.normal('-')[0]
+                             + uv_av[1]*self.normal('-')[1])
+                    s = 0.5*(sign(un_av) + 1.0)
+                    c_up = solution('-')*s + solution('+')*(1-s)
 
-                # Interface term
-                f = i * c_up*(jump(uv, self.normal)) * adjoint('+') * self.dS
-                # TODO: Make symmetric (+ and - are arbitrary)
+                    # Interface term
+                    loc = i * dot(uv, self.normal) * adjoint
+                    f += c_up * (loc('+') + loc('-')) * self.dS
 
-                # Term resulting from reverse integration by parts
-                f += -i * dot(uv('+'), self.normal('+')) * solution('+') * adjoint('+') * self.dS
-                # TODO: Make symmetric (+ and - are arbitrary)
+                    # Term resulting from second integration by parts
+                    loc = -i * dot(uv, self.normal) * solution * adjoint
+                    f += (loc('+') + loc('-')) * self.dS + loc * ds(degree=self.quad_degree)
 
-                # TODO: Lax-Friedrichs
+                    # TODO: Lax-Friedrichs
 
-                if bnd_conditions is not None:
-                    for bnd_marker in self.boundary_markers:
-                        funcs = bnd_conditions.get(bnd_marker)
-                        ds_bnd = ds(int(bnd_marker), degree=self.quad_degree)
-                        c_in = solution
-                        if funcs is not None:
-                            c_ext, uv_ext, eta_ext = self.get_bnd_functions(c_in, uv, elev, bnd_marker, bnd_conditions)
-                            uv_av = 0.5*(uv + uv_ext)
-                            un_av = self.normal[0]*uv_av[0] + self.normal[1]*uv_av[1]
-                            s = 0.5*(sign(un_av) + 1.0)
-                            c_up = (c_in-c_ext)*(1-s)  # c_in-c_ext is boundary residual
-                            f += I * c_up*(uv_av[0]*self.normal[0]
-                                           + uv_av[1]*self.normal[1]) * adjoint * ds_bnd
-            # TODO: Check works with CG space
-            return -f
+                    if bnd_conditions is not None:
+                        for bnd_marker in self.boundary_markers:
+                            funcs = bnd_conditions.get(bnd_marker)
+                            ds_bnd = ds(int(bnd_marker), degree=self.quad_degree)
+                            c_in = solution
+                            if funcs is not None:
+                                c_ext, uv_ext, eta_ext = self.get_bnd_functions(c_in, uv, elev, bnd_marker, bnd_conditions)
+                                uv_av = 0.5*(uv + uv_ext)
+                                un_av = self.normal[0]*uv_av[0] + self.normal[1]*uv_av[1]
+                                s = 0.5*(sign(un_av) + 1.0)
+                                c_up = (c_in-c_ext)*(1-s)  # c_in-c_ext is boundary residual
+                                f += i * c_up*(uv_av[0]*self.normal[0]
+                                               + uv_av[1]*self.normal[1]) * adjoint * ds_bnd
+                return -f
 
 
 class HorizontalDiffusionResidual(TracerTerm):
@@ -89,17 +88,17 @@ class HorizontalDiffusionResidual(TracerTerm):
 
     def residual_cell(self, solution, solution_old, fields, fields_old, bnd_conditions=None, adjoint=None):
         if fields_old.get('diffusivity_h') is not None:
-            P0 = FunctionSpace(solution.function_space().mesh(), "DG", 0)
-            I = TestFunction(P0)
+            i = TestFunction(FunctionSpace(self.mesh, "DG", 0))
             diffusivity_h = fields_old['diffusivity_h']
             diff_tensor = as_matrix([[diffusivity_h, 0, ],
                                      [0, diffusivity_h, ]])
             diff_flux = dot(diff_tensor, grad(solution))
 
+            f = 0
             if adjoint is None:
                 f = -div(diff_flux)
             else:
-                f = -I * div(diff_flux) * adjoint * self.dx
+                f += -i * div(diff_flux) * adjoint * self.dx
 
             return -f
 
@@ -107,33 +106,31 @@ class HorizontalDiffusionResidual(TracerTerm):
         if adjoint is None:
             raise NotImplementedError  # TODO
         else:
-            P0 = FunctionSpace(solution.function_space().mesh(), "DG", 0)
-            I = TestFunction(P0)
-            i = avg(I)
-            diffusivity_h = fields_old['diffusivity_h']
-            diff_tensor = as_matrix([[diffusivity_h, 0, ],
-                                         [0, diffusivity_h, ]])
+            f = 0
+            if self.horizontal_dg:
+                i = TestFunction(FunctionSpace(self.mesh, "DG", 0))
+                diffusivity_h = fields_old['diffusivity_h']
+                diff_tensor = as_matrix([[diffusivity_h, 0, ],
+                                             [0, diffusivity_h, ]])
 
-            f = I * inner(dot(diff_tensor, grad(solution)), self.normal) * adjoint * ds
+                degree_h = self.function_space.ufl_element().degree()
+                sigma = 5.0*degree_h*(degree_h + 1)/self.cellsize
+                if degree_h == 0:
+                    sigma = 1.5 / self.cellsize
+                alpha = avg(sigma)
 
-            degree_h = self.function_space.ufl_element().degree()
-            sigma = 5.0*degree_h*(degree_h + 1)/self.cellsize
-            if degree_h == 0:
-                sigma = 1.5 / self.cellsize
-            alpha = avg(sigma)
-            ds_interior = self.dS
+                loc = i * self.normal * adjoint
+                f += alpha*inner(dot(avg(diff_tensor), jump(solution, self.normal)),
+                                 loc('+') + loc('-')) * self.dS
+                f += -inner(jump(dot(diff_tensor, grad(solution))),
+                            loc('+') + loc('-')) * self.dS
+                loc = i * dot(diff_tensor, grad(adjoint))
+                f += -0.5 *  inner(loc('+') + loc('-'),
+                                   jump(solution, self.normal)) * self.dS
 
-            f += i * alpha*inner(dot(avg(diff_tensor), jump(solution, self.normal)),
-                                self.normal('+'))*adjoint('+')*ds_interior
-            # TODO: Make symmetric (+ and - are arbitrary)
-            f += -0.5 * i * inner(dot(diff_tensor, grad(adjoint('+'))),
-                                  jump(solution, self.normal))*ds_interior
-            # TODO: Make symmetric (+ and - are arbitrary)
-            f += 0.5 * i * inner(jump(dot(diff_tensor, grad(solution))),
-                            self.normal('+'))*adjoint('+')*ds_interior
-            # TODO: Make symmetric (+ and - are arbitrary)
+                loc = i * dot(dot(diff_tensor, grad(solution)), self.normal) * adjoint
+                f += (loc('+') + loc('-')) * self.dS + loc * ds(degree=self.quad_degree)
 
-            # TODO: Check works with CG space
             return -f
 
 
@@ -145,17 +142,17 @@ class SourceResidual(TracerTerm):
 
     def residual_cell(self, solution, solution_old, fields, fields_old, bnd_conditions=None, adjoint=None):
         source = fields_old.get('source')
+        f = 0
         if source is not None:
             if adjoint is None:
                 f = source
             else:
-                P0 = FunctionSpace(solution.function_space().mesh(), "DG", 0)
-                I = TestFunction(P0)
-                f = I * source * adjoint * self.dx
-            return f
+                i = TestFunction(FunctionSpace(self.mesh, "DG", 0))
+                f += i * source * adjoint * self.dx
+        return f
 
     def residual_edge(self, solution, solution_old, fields, fields_old, bnd_conditions=None, adjoint=None):
-        return None
+        return 0
 
 
 class TracerResidual2D(Equation):
